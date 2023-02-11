563274a3eceddce323ca24a972d99154c6b6d0c3
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-02-11 21:53:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,55 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map( function (value) {
+        var b = Object.keys(value);
+        b.map(function (d) {
+            console.log(d , value[d]);
+        })
+    
+    })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   arr.forEach((value) => {
+        var e = Object.keys(value);
+        e.forEach((f) => {
+            console.log(f , value[f]);
+        })
+    })
 }
 
 function addData() {
   //Write your code here, just console.log
+  var h =  {id:4,name:"susan",age:"20", 
+   marks:45}
+   arr.push(h);
+   var j = Object.keys(arr[3]);
+   j.forEach((m) => {
+    console.log(m , arr[3][m])
+   })
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  var n = arr.length;
+    for (var i=0;i<n;i++){
+        if(arr[i].marks<50){
+            arr.splice(i , 1);
+        }
+    }
+    console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  var o = [
+        { id: 7, name: "john", age: "18", marks: 08 },
+        { id: 5, name: "jack", age: "20", marks: 55 },
+        { id: 9, name: "karen", age: "19", marks: 58 },
+      ];
+   
+      var r = arr.concat(o);
+            console.log(r); 
 }
